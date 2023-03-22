203f9efbecafa7ca84a8293aff2bbb9893ee7b56
Hridoy Banik <hridoy.banik71@gmail.com>
2023-03-23 00:55:25 +0600

    add: add serch api call

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,16 +1,49 @@
+import { PrismaClient } from "@prisma/client";
 import React from "react";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-const Search = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    slug: true,
+    location: true,
+  };
+  if (!city) return prisma.restaurant.findMany({ select });
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city,
+        },
+      },
+    },
+    select,
+  });
+};
+
+const Search = async ({ searchParams }: { searchParams: { city: string } }) => {
+  const restaurants = await fetchRestaurantsByCity(
+    searchParams.city.toLowerCase()
+  );
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            <RestaurantCard />
+          ) : (
+            <p>Sorry no restaurant found in this city</p>
+          )}
         </div>
       </div>
     </>
